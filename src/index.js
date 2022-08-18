c3bfa8c55f1ea5d0ae694add6d2c37f6314f7c84
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-08-18 19:00:46 +0600

    dynamically create signed msg

--- src/index.js
+++ src/index.js
@@ -17,17 +17,7 @@ router
 		const body = await request.json();
 
 		const walletAddress = body.walletAddress?.trim();
-		const signedMsg = body.signedMsg?.trim();
-		const walletSignature = body.walletSignature?.trim();
-
-		const signerAddress = recoverPersonalSignature({
-			data: signedMsg,
-			signature: walletSignature
-		  });
-
-		if (walletAddress !== signerAddress) {
-			return new Response(null, { status: 401 });
-		}
+		const walletSignature = body.walletSignature?.trim()
 
 		const name = body.name?.trim();
 		const email = body.email?.trim();
@@ -36,6 +26,18 @@ router
 
 		if (!walletAddress || !walletSignature || !name || !businessName || !(email || phone)) {
 			return new Response('Required body parameter is missing or invalid', { status: 400 });
+		}
+
+		const signedMsg = createHexMsg(name, businessName, phone, email);
+		console.log(signedMsg);
+
+		const signerAddress = recoverPersonalSignature({
+			data: signedMsg,
+			signature: walletSignature
+		  });
+
+		if (walletAddress !== signerAddress) {
+			return new Response(null, { status: 401 });
 		}
 
 		const digestBuffer = await crypto.subtle.digest(
@@ -57,7 +59,11 @@ router
 	})
 	.all('*', async () => new Response(null, { status: 404 }));
 
-
 addEventListener("fetch", (event) => {
 	event.respondWith(router.handle(event.request).catch((err) => new Response(err.stack, { status: 400 })));
 });
+
+function createHexMsg(name, businessName, phone, email) {
+	const msg = `My name is ${name}. My business name is ${businessName}. My phone is ${phone}. My email is ${email}.`;
+	return `0x${Buffer.from(msg, "utf8").toString("hex")}`;
+};
